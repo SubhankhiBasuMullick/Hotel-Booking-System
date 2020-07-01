dbf47ad3fb5fba0aca83472f6b88944659138c96
SubhankhiBasuMullick <subhankhibasumullick@gmail.com>
2020-07-01 22:31:27 +0530

    initial commit

--- project/js/validation.js
+++ project/js/validation.js
@@ -1,20 +1,61 @@
-$('.slider span.next').click(function() {
-  $current = $(this).siblings('img.active');
-  $next = $current.next('img');
-  if ($next.length != 0) {
-    $current.removeClass('active');
-    $next.addClass('active');
-  }
-})
-$('.slider span.prev').click(function() {
-  $current = $(this).siblings('img.active');
-  $prev = $current.prev('img');
-  if ($prev.length != 0) {
-    $current.removeClass('active');
-    $prev.addClass('active');
-  }
-})
+function validateRegForm() {
+  var fname = document.getElementById("firstname").value;
+  var lname = document.getElementById("lastname").value;
+  var uname = document.getElementById("username").value;               
+var phone = document.getElementById("telephone").value;  
+if (fname == "")                                  
+{ 
+window.alert("Please enter your first name."); 
+fname.focus();
+return false;
+}
+else if(isNaN(fname) /*"%d[10]"*/)
+{
+alert("First name confirmed");
+}
+else{ 
+window.alert("please enter character"); 
+}   
 
+if (lname == "")                                  
+{ 
+window.alert("Please enter your last name."); 
+lname.focus();
+return false;
+}
+else if(isNaN(lname) /*"%d[10]"*/)
+{
+alert(" Last name confirmed");
+}
+else{ 
+window.alert("please enter character"); 
+}   
 
-
+if (uname == "")                                  
+{ 
+window.alert("Please enter your user name."); 
+uname.focus();
+return false;
+}
+else if(isNaN(uname) /*"%d[10]"*/)
+{
+alert("User name confirmed");
+}
+else{ 
+window.alert("please enter character"); 
+}   
+if (phone == "")                           
+{ 
+window.alert("Please enter your telephone number."); 
+phone.focus();
+return false; 
+} 
+else if(!isNaN(phone) /*phone.value == isNaN(phone.value)*/)
+{
+alert("number confirmed");
+}
+else{
+window.alert("please enter numbers only");
+}   
+}
 
